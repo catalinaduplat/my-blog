936a7d9f6f840a054ba00509ed29dbfc3947186c
Catalina Duplat <Catalina.DuplatMolano@adidas.com>
2020-04-26 15:21:24 -0500

    First commit

---
+++ src/build/precache-manifest.468c6856b5fef380d831440ed77f9ff5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a2647df7366823bba2e0d9fa0719b76",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d8bfe8ed6a7674e41a7c",
+    "url": "/static/css/main.7b5f7dc8.chunk.css"
+  },
+  {
+    "revision": "eb21b5816088ede3a3f6",
+    "url": "/static/js/2.8d356d5a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8d356d5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8bfe8ed6a7674e41a7c",
+    "url": "/static/js/main.1bc80bba.chunk.js"
+  },
+  {
+    "revision": "e988c064c70788dbc50e",
+    "url": "/static/js/runtime-main.a56884a5.js"
+  }
+]);
